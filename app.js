6697b5c7a93bb9c5ddfb6aede604b35298982c87
Axel Cuevas <axelcoevas@gmail.com>
2021-07-07 21:38:31 -0500

    Añadiendo envío de correo

---
+++ app.js
@@ -0,0 +1,52 @@
+$("[data-toggle=popover]").popover();
+
+const forms = document.querySelectorAll(".signup-form");
+
+const getTemplate = () => {
+  return fetch("./template.html").then((response) => response.text());
+};
+
+const sendEmailToApi = (address, template) => {
+  fetch(`https://bedu-email-sender-api.herokuapp.com/send?id`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      address: address,
+      template: template,
+    }),
+  })
+    .then((results) => {
+      console.log(results.status);
+      if (results.status == 200) {
+        alert("E-mail send!!!");
+      } else {
+        alert("Send failed");
+      }
+      document.getElementById("email").value = "";
+    })
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("email").value = "";
+      alert("Send failed");
+    });
+};
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  const email = e.target.querySelector("input").value;
+  getTemplate()
+    .then((template) => {
+      sendEmailToApi(email, template);
+    })
+    .catch((error) => {
+      console.log(error, "error al obtener el template");
+    });
+};
+
+
+
+for (let i = 0; i < forms.length; i++) {
+  forms[i].addEventListener("submit", sendEmail);
+}
